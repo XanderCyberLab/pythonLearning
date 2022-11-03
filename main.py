783fe7d206ca445f4ee9114baf23f1fbab571118
Wishymashy <amoralesmm@yahoo.com>
2022-11-03 11:06:46 -0500

    live span calculation

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 print("Hello Alexander")
 age = input("Please enter your age ")
 print(age)
+int_age = int(age)
+days = 365 * int_age
+weeks = 52 * int_age
+months = 12 * int_age
+print(f"You total live span in days are {days}, in weeks {weeks}, and in months {months}")
