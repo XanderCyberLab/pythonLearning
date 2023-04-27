a486cdb2da154d4d5d3c7b1e971b2ad9f9aeb163
Wishymashy <amoralesmm@yahoo.com>
2023-04-27 11:42:19 -0500

    Simplified Leap Year Calc

--- leap_year.py
+++ leap_year.py
@@ -1,8 +1,13 @@
 year = int(input("Enter the Year to determine if it is a Leap Year: "))
 
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print(f"{year} is a Leap Year")
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print(f"{year} is a Leap Year")
+# else:
+#     print(f"{year} is not a Leap Year")
+    
+if year % 4 == 0 & year % 100 == 0 & year % 400 == 0:
+    print(f"{year} is a Leap Year")
 else:
     print(f"{year} is not a Leap Year")
