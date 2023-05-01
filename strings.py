94920302ebc0e3bed8ce76eddd65128071676127
Wishymashy <amoralesmm@yahoo.com>
2023-05-01 10:53:27 -0500

    String Methods

---
+++ strings.py
@@ -0,0 +1,16 @@
+hello = "hello HELLO"
+print(hello.upper())
+print(hello.lower())
+print(hello.capitalize())
+print(hello.lower().count("l"))
+
+x = "Hello"
+y = "World"
+print(x + " " + y)
+
+x = ("Alex")
+
+if x == "Alex":
+    print("Hello " + x)
+else:
+    print("Hello World")
