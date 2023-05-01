e0eb629a65fd23405c4d62d20dfa533e4f09977e
Wishymashy <amoralesmm@yahoo.com>
2023-05-01 12:01:26 -0500

    Loops and Dictionaries

---
+++ loops.py
@@ -0,0 +1,42 @@
+# for i in range(10,  -1, -1):
+#     print(i)
+
+
+# for i in [3,4,52,66,76]:
+#     print(i)
+
+x = [1,3,5,7,9, 14,155,123,1,62,33,26,21]
+
+# for i in range(len(x)):
+#     print(x[i])
+
+# for i, element in enumerate(x):
+#     print(i, element)
+
+# i = 0
+
+# while i < 10:
+#     print('run')
+#     i += 1
+#     if i == 10: 
+#         break
+
+
+#slicing
+# sliced = x[0:2:1]
+# print(sliced)
+
+# Set
+# x = set()
+# s = {4,22,22,5,1,75,3}
+# print(s)
+
+# #Dictionary
+# x = {'key': 10}
+
+# print(x["key"])
+
+# print('key' in x)
+
+# for key, value in x.items():
+#     print(key, value)
