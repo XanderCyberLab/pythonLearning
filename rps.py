cbc3ebe4bebe7ae9f4fed9fde278f57fe428da4f
Wishymashy <amoralesmm@yahoo.com>
2023-05-03 13:56:48 -0500

    RPS Completed

--- rps.py
+++ rps.py
@@ -1,6 +1,7 @@
 import random
-player_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
-computer_random = random.randint(0,2)
+player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n"))
+computer_choice = random.randint(0,2)
+
 
 rock = '''
     _______
@@ -29,12 +30,38 @@ scissors = '''
 ---.__(___)
 '''
 
-if player_choice == "0":
-    print(rock)
-    
-elif player_choice == "1":
-    print(paper)
-    
-else:
+print("Player choice: ")
+if player_choice == 0:
+    print(rock)    
+if player_choice == 1:
+    print(paper)    
+if player_choice == 2:
     print(scissors)
+print("Computer choice: ")
+if computer_choice == 0:
+    print(rock)
+if computer_choice == 1:
+    print(paper)
+if computer_choice == 2:
+    print(scissors)
+
+if player_choice == computer_choice:
+    print("Tie!")
+elif player_choice == 0:
+    if computer_choice == 1:
+        print("You lose! Paper covers rock.")
+    else:
+        print("You win! Rock smashes scissors.")
+elif player_choice == 1:
+    if computer_choice == 2:
+        print("You lose! Scissors cut paper.")
+    else:
+        print("You win! Paper covers rock.")
+elif player_choice == 2:
+    if computer_choice == 0:
+        print("You lose! Rock smashes scissors.")
+    else:
+        print("You win! Scissors cut paper.")
+else:
+    print("Invalid choice. Please enter either rock, paper, or scissors.")
     
